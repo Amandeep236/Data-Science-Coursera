6971049757d163ada43447fc3db71a8f71ddd253
HY-CHENG <hsinyu.cheng2015@gmail.com>
2015-05-26 20:37:27 -0600

    0526 update

--- 2.R Programming/Assignment 3_Hospital Quality/best.R
+++ 2.R Programming/Assignment 3_Hospital Quality/best.R
@@ -1,4 +1,13 @@
 
+## Part 1 : Finding the best hospital in a state
+
+## Write a function called best that take two arguments: the 2-character abbreviated name of a state and an
+## outcome name. The function reads the outcome-of-care-measures.csv and returns a character vector
+## with the name of the hospital that has the best (i.e. lowest) 30-day mortality for the specised outcome
+## in that state. The hospital name is the name provided in the Hospital.Name variable. The outcomes can
+## be one of \heart attack", \heart failure", or \pneumonia". Hospitals that do not have data on a particular
+## outcome should be excluded from the set of hospitals when deciding the rankings.
+
 
 best <- function(state, outcome) {    
     
@@ -36,4 +45,30 @@ best <- function(state, outcome) {
     
     Result[1,]
 } 
+
+
+## Test
+
+best("TX", "heart attack")
+#"CYPRESS FAIRBANKS MEDICAL CENTER"
+
+best("TX", "heart failure")
+#"FORT DUNCAN MEDICAL CENTER"
+
+best("MD", "heart attack")
+#"JOHNS HOPKINS HOSPITAL, THE"
+
+best("MD", "pneumonia")
+#"GREATER BALTIMORE MEDICAL CENTER"
+
+best("BB", "heart attack")
+#Error in best("BB", "heart attack") : invalid state
+
+best("NY", "hert attack")
+#Error in best("NY", "hert attack") : invalid outcome
+
+
+
+
+
 
--- 2.R Programming/Assignment 3_Hospital Quality/rankhospital.R
+++ 2.R Programming/Assignment 3_Hospital Quality/rankhospital.R
@@ -1,4 +1,17 @@
 
+## Part 2 : Ranking hospitals by outcome in a state
+
+## Write a function called rankhospital that takes three arguments: the 2-character abbreviated name of a
+## state (state), an outcome (outcome), and the ranking of a hospital in that state for that outcome (num).
+
+## The function reads the outcome-of-care-measures.csv and returns a character vector with the name
+## of the hospital that has the ranking specised by the num argument. 
+
+## For example, the call rankhospital("MD", "heart failure", 5)
+## would return a character vector containing the name of the hospital with the 5th lowest 30-day death rate
+## for heart failure. The num argument can take values \best", \worst", or an integer indicating the ranking
+## (smaller numbers are better).
+
 
 rankhospital <- function(state, outcome, num = "best") {    
     
@@ -50,18 +63,13 @@ rankhospital <- function(state, outcome, num = "best") {
 
 
 
+## Test
 
+rankhospital("TX", "heart failure", 4)
+# "DETAR HOSPITAL NAVARRO"
 
+rankhospital("MD", "heart attack", "worst")
+# "HARFORD MEMORIAL HOSPITAL"
 
-
-
-
-
-
-
-
-
-
-
-
-
+rankhospital("MN", "heart attack", 5000)
+# NA
